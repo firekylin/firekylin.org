594e20ea6fc14a483009e3b1c75843b654bb5d55
lizheming <secretlzm007@gmail.com>
2017-03-18 18:43:33 +0800

    add changelog and version

--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -1,13 +1,22 @@
 'use strict';
 
 import Base from './base.js';
+import path from 'path';
+import fs from 'fs';
+
+const readFileAsync = think.promisify(fs.readFile, fs);
 
 export default class extends Base {
   /**
    * index action
    * @return {Promise} []
    */
-  indexAction(){
+  async indexAction(){
+    let release = path.join(think.RESOURCE_PATH, 'release/.latest');
+    let version = false;
+
+    version = await readFileAsync(release).catch(() => false);
+    this.assign({latest: version});
     //auto render template file index_index.html
     return this.display();
   }
